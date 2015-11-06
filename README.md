fec1f2bf0897cc730bf71e018fcf804afa77cde5
John Thompson <fly.yamaha@gmail.com>
2015-11-06 12:57:08 -0500

    Adding files from initial project create.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Spring Core - Spring MVC
+
+Source code in this repo is to support my on line course for the Spring Framework. 
+
+You can learn more about my course [here](http://courses.springframework.guru/courses/spring-core/).
---
+++ src/main/java/guru/springframework/SpringmvcApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcApplication.class, args);
+    }
+}

b7b97e900f53bb84f0059fc6dcb70aec48814be5
John Thompson <john@springframework.guru>
2015-11-09 18:00:46 -0500

    Added Create a Product

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -38,6 +38,12 @@ public class ProductController {
         return "product";
     }
 
+    @RequestMapping("product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+        return "productform";
+    }
+
     @RequestMapping("/product/new")
     public String newProduct(Model model){
         model.addAttribute("product", new Product());

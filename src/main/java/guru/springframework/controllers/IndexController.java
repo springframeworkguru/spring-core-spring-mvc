5f25fd987a5f4adbcf30f153b717d504f09ffa78
John Thompson <john@springframework.guru>
2015-11-20 17:12:08 -0500

    fixed URL mapping issue for index

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping({"/", ""})
     public String index(){
         return "index";
     }

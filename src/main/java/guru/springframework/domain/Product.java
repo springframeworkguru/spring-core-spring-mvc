6ad1014da413fb214119c72218cebbbbfc6b8d5e
John Thompson <john@springframework.guru>
2015-12-09 12:30:47 -0500

    Converted Product to JPA Entity

--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -1,9 +1,6 @@
 package guru.springframework.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
@@ -15,6 +12,10 @@ public class Product implements DomainObject{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String description;
     private BigDecimal price;
     private String imageUrl;
@@ -25,6 +26,14 @@ public class Product implements DomainObject{
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getDescription() {

b916f223e59c3b18e6a406ec8136060bafe66470
John Thompson <john@springframework.guru>
2016-01-07 18:18:10 -0500

    added config for Spring Security

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -17,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * Created by jt on 12/28/15.
  */
 @Configuration
+@EnableWebSecurity
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
 
     private AuthenticationProvider authenticationProvider;

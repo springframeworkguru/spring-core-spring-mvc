bf5247a3443727530397e7607ee503f6453fd853
John Thompson <john@springframework.guru>
2016-01-19 15:53:36 -0500

    added config for Spring AOP

---
+++ src/main/java/guru/springframework/config/AopConfig.java
@@ -0,0 +1,12 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by jt on 1/7/16.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+}

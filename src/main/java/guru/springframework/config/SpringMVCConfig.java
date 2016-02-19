2eaad32adbb07e8a0109bca7c86d263b45ac14f4
John Thompson <john@springframework.guru>
2016-02-19 17:36:05 -0500

    Added config for Spring MVC

---
+++ src/main/java/guru/springframework/config/SpringMVCConfig.java
@@ -0,0 +1,12 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by jt on 2/19/16.
+ */
+@EnableWebMvc
+@Configuration
+public class SpringMVCConfig {
+}

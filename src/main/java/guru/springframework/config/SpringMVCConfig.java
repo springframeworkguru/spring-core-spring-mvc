107af2731171034663a880d1b6c61e973d686b6c
John Thompson <john@springframework.guru>
2016-04-17 17:26:26 -0400

    added config for web resources

--- src/main/java/guru/springframework/config/SpringMVCConfig.java
+++ src/main/java/guru/springframework/config/SpringMVCConfig.java
@@ -2,11 +2,19 @@ package guru.springframework.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by jt on 2/19/16.
  */
 @EnableWebMvc
 @Configuration
-public class SpringMVCConfig {
+public class SpringMVCConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+    }
 }
